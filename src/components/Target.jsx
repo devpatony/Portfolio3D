267037b48fee912a50dd5a6d431ed168033fb184
dev <techdep205@gmail.com>
2025-02-05 18:59:47 +0530

    add GSAP animations and new components for enhanced interactivity

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,6 +1,6 @@
 import { useGLTF } from '@react-three/drei';
 import { useRef } from 'react';
-import { useGSAP } from '@gsap/react';
+import { useGSAP } from "@gsap/react";
 import gsap from 'gsap';
 
 const Target = (props) => {
